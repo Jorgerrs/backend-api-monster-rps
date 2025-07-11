bb72df826347d94f9985f6bb0cc572cf0a08eb30
Jorgerrs <jordixl90@gmail.com>
2025-07-11 19:24:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 // MongoDB connection
 const connectDB = async () => {
